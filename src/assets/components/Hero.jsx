9570d3ae88230f8ef31512f47d7825ce0fc1cd86
Brendan McBride <brendan.mcmath@gmail.com>
2023-02-28 06:30:36 +0800

    feat: add nav and hero to page

---
+++ src/assets/components/Hero.jsx
@@ -0,0 +1,9 @@
+function Hero() {
+	return (
+		<div className="hero" style={{display: "flex", justifyContent: "center", paddingTop: "4rem"}}>
+			<img src="/src/assets/images/photo-grid.png" alt="grid of photos" style={{maxHeight: "500px"}} />
+		</div>
+	)
+}
+
+export default Hero
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+	return (
+	 <nav className="navbar" style={{display: "flex", backgroundColor: "#fefefe", paddingTop: "2rem", paddingBottom: "2rem", width: "100vw", boxShadow: "0 1px 15px #d3d3d3"}}>
+		<img src="/src/assets/images/airbnb-logo.png" alt="airbnb logo" style={{marginLeft: "4rem", height: "40px"}} />
+	 </nav>
+	)
+}
+
+export default Navbar
